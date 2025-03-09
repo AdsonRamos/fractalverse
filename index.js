ca1e7267ebb8187d7d45dcc9e69535454f74f324
adsonramos <adsonpaulo.95@gmail.com>
2025-03-09 16:29:36 -0300

    chore: adding fractal tree

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const fractals = [
   { name: "Triângulo de Sierpiński", description: "Triângulo subdividido removendo o centro.", img: 'resources/fractals/sierpinski_triangle.png', link: 'src/pages/sierpisnki_triangle.html' },
   { name: "Carpete de Sierpiński", description: "Quadrado dividido em 9 partes, removendo o centro.", img: 'resources/fractals/sierpinski_carpet.png', link: 'src/pages/sierpinski_carpet.html' },
   { name: "Curva de Koch", description: "Segmento substituído por um padrão em forma de 'V'.", img: 'resources/fractals/koch_curve.png', link: 'src/pages/koch_curve.html' },
-  { name: "Floco de Neve de Koch", description: "Triângulo com cada lado transformado em curva de Koch.", img: 'resources/fractals/koch_snowflake.png', link: 'src/pages/koch_snowflake.html'},
-  /* { name: "Árvore de Pythagoras", description: "Quadrado gerando dois quadrados inclinados recursivamente." },
-  { name: "Conjunto de Mandelbrot", description: "Definido por \( z_{n+1} = z_n^2 + c \), iterando no plano complexo." },
-  { name: "Conjunto de Julia", description: "Similar ao Mandelbrot, mas fixando \( c \) e variando \( z_0 \)." },
-  { name: "Curva de Dragon", description: "Segmento dobrado repetidamente em ângulos retos." },
-  { name: "Curva de Hilbert", description: "Linha preenchendo o espaço com curvas em 'U' recursivas." },
-  { name: "Curva de Peano", description: "Curva que preenche o espaço contínuo subdividindo-se." },
-  { name: "Curva de Gosper", description: "Curva fractal suave baseada em ângulos diferentes." },
-  { name: "Samambaia de Barnsley", description: "Gerado por transformações geométricas iteradas." },
-  { name: "Triângulo de Vicsek", description: "Quadrado subdividido e removendo áreas centrais." } */
+  { name: "Floco de Neve de Koch", description: "Triângulo com cada lado transformado em curva de Koch.", img: 'resources/fractals/koch_snowflake.png', link: 'src/pages/koch_snowflake.html' },
+  { name: "Árvore Fractal", description: "Árvore gerada a partir de um galho inicial que se reparte em outros dois.", img: 'resources/fractals/fractal_tree.png', link: 'src/pages/fractal_tree.html' },
+  /* { name: "Árvore de Pythagoras (Consertar)", description: "Quadrado gerando dois quadrados inclinados recursivamente.", img: 'resources/fractals/pythagoras_tree.png', link: 'src/pages/pythagoras_tree.html' }, */
+  { name: "Conjunto de Mandelbrot", description: "Definido por \( z_{n+1} = z_n^2 + c \), iterando no plano complexo.", img: 'resources/fractals/mandelbrot_set.png', link: 'src/pages/mandelbrot_set.html' },
+  /* { name: "Conjunto de Julia", description: "Similar ao Mandelbrot, mas fixando \( c \) e variando \( z_0 \).", img: '', link: '' },
+  { name: "Curva de Dragon", description: "Segmento dobrado repetidamente em ângulos retos.", img: '', link: '' },
+  { name: "Curva de Hilbert", description: "Linha preenchendo o espaço com curvas em 'U' recursivas.", img: '', link: '' },
+  { name: "Curva de Peano", description: "Curva que preenche o espaço contínuo subdividindo-se.", img: '', link: '' },
+  { name: "Curva de Gosper", description: "Curva fractal suave baseada em ângulos diferentes.", img: '', link: '' },
+  { name: "Samambaia de Barnsley", description: "Gerado por transformações geométricas iteradas.", img: '', link: '' },
+  { name: "Triângulo de Vicsek", description: "Quadrado subdividido e removendo áreas centrais.", img: '', link: '' } */
 ];
 
 const container = document.getElementById("cards-container");
