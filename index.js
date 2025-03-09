5dbb25bb6651d04c9b79b8260ae296a1664d357a
adsonramos <adsonpaulo.95@gmail.com>
2025-03-09 16:41:12 -0300

    chore: adding dragon curve

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const fractals = [
   /* { name: "Árvore de Pythagoras (Consertar)", description: "Quadrado gerando dois quadrados inclinados recursivamente.", img: 'resources/fractals/pythagoras_tree.png', link: 'src/pages/pythagoras_tree.html' }, */
   { name: "Conjunto de Mandelbrot", description: "Definido por \( z_{n+1} = z_n^2 + c \), iterando no plano complexo.", img: 'resources/fractals/mandelbrot_set.png', link: 'src/pages/mandelbrot_set.html' },
   { name: "Conjunto de Julia", description: "Similar ao Mandelbrot, mas fixando \( c \) e variando \( z_0 \).", img: 'resources/fractals/julia_set.png', link: 'src/pages/julia_set.html' },
-  /* { name: "Curva de Dragon", description: "Segmento dobrado repetidamente em ângulos retos.", img: 'resources/fractals/', link: 'src/pages/'},
-  { name: "Curva de Hilbert", description: "Linha preenchendo o espaço com curvas em 'U' recursivas.", img: 'resources/fractals/', link: 'src/pages/'},
+  { name: "Curva de Dragon", description: "Segmento dobrado repetidamente em ângulos retos.", img: 'resources/fractals/dragon_curve.png', link: 'src/pages/dragon_curve.html'},
+  /* { name: "Curva de Hilbert", description: "Linha preenchendo o espaço com curvas em 'U' recursivas.", img: 'resources/fractals/', link: 'src/pages/'},
   { name: "Curva de Peano", description: "Curva que preenche o espaço contínuo subdividindo-se.", img: 'resources/fractals/', link: 'src/pages/'},
   { name: "Curva de Gosper", description: "Curva fractal suave baseada em ângulos diferentes.", img: 'resources/fractals/', link: 'src/pages/'},
   { name: "Samambaia de Barnsley", description: "Gerado por transformações geométricas iteradas.", img: 'resources/fractals/', link: 'src/pages/'},
